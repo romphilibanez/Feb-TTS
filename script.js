1e66f43e4d6b92eaed87eb3944b63aee6ffd9926
Joseph Ibanez <69676508+romphilibanez@users.noreply.github.com>
2025-03-04 15:37:00 +0800

    Add files via upload
    landing page

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector('.search-button').addEventListener('click', function() {
+    alert('This is a mock search page. Results are static.');
+});
+
+document.querySelector('.search-labs').addEventListener('click', function() {
+    alert('Search Labs is not available in this demo.');
+});
+
+document.querySelector('.google-apps').addEventListener('click', function() {
+    alert('Google Apps menu is not functional in this mock-up.');
+});
+
+document.querySelector('.profile-pic').addEventListener('click', function() {
+    alert('Profile settings are disabled in this demo.');
+});
